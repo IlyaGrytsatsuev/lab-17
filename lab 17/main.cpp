163c24e43ca4f279bb6e2eac9f435228d73944af
IlyaGrytsatsuev <ilya.gr2003@yandex.ru>
2021-09-11 23:15:03 +0300

    Initial Commit

---
+++ lab 17/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  lab 17
+//
+//  Created by Илья Грицацуев on 11.09.2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
